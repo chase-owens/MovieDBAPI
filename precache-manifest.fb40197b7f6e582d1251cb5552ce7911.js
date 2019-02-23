a8a486f67b0ff845fc47a36feb8c01ee7a512a4b
Chase-Owens <chasejonathanowens@gmail.com>
2019-02-23 15:57:15 -0600

    Updates

---
+++ precache-manifest.fb40197b7f6e582d1251cb5552ce7911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32bf25a8edfcf223b983",
+    "url": "/MovieDBAPI/static/js/runtime~main.32bf25a8.js"
+  },
+  {
+    "revision": "1761bdd451b9fc5d949d",
+    "url": "/MovieDBAPI/static/js/main.1761bdd4.chunk.js"
+  },
+  {
+    "revision": "d097625e7b2566798c3b",
+    "url": "/MovieDBAPI/static/js/2.d097625e.chunk.js"
+  },
+  {
+    "revision": "1761bdd451b9fc5d949d",
+    "url": "/MovieDBAPI/static/css/main.2060be9b.chunk.css"
+  },
+  {
+    "revision": "0e4c4425eafb14e0e0f4942015ddbdaf",
+    "url": "/MovieDBAPI/index.html"
+  }
+];
